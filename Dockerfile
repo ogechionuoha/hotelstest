653127c00b2b77ae48a859f1f89cc309c0204e12
Ogecho Onuoha <oge.blessing@live.com>
2020-10-25 21:27:33 +0000

    upate setup script and docker file

--- Dockerfile
+++ Dockerfile
@@ -4,43 +4,24 @@ LABEL maintainer="oge.blessing@gmail.com"
 
 #install relevant libs
 RUN apt-get install wget
-RUN apt install libgl1-mesa-glx
-RUN apt-get update
-RUN apt-get install vim
+RUN apt install -y libgl1-mesa-glx 
+RUN apt install -y liblz4-tool
+RUN apt-get -y update
+RUN apt-get -y install vim
 
 #Copy files
 WORKDIR /
 RUN mkdir /HOTELS-50K
 WORKDIR /HOTELS-50K
+RUN mkdir pretrained_model
 COPY . .
-
-#VOLUME ./input ./images
 
 #install dependencies 
 RUN pip install -r requirements.txt
 
 #install other dependencies
 RUN pip install --upgrade tf_slim
-RUN pip install lz4tools 
 RUN pip install faiss-gpu
 
-#Extract files --optionally remove run on start of container
-#RUN tar xvzf ./input/dataset.tar.gz -C ./input
-
-#download test images to images folder to preserve structure.
-RUN wget https://cs.slu.edu/~stylianou/images/hotels-50k/test.tar.lz4 -P ./images/ --no-check-certificate
-#--optionally remove and run on start of container
-#RUN lz4toolsCli -d ./images/test.tar.lz4
-#RUN tar xf ./images/test.tar -C ./images
-
-#Get pretrained model
-WORKDIR /HOTELS-50K
-#VOLUME ./pretrained_model
-WORKDIR /HOTELS-50K/pretrained_model
-RUN wget https://www2.seas.gwu.edu/~astylianou/hotels50k/hotels50k_snapshot.tar.gz
-#--optionally remove and run on start of container
-#RUN tar xvzf hotels50k_snapshot.tar.gz
-
-
-WORKDIR /HOTELS-50K
+#start command
 ENTRYPOINT [ "/bin/bash" ]
--- setup.sh
+++ setup.sh
@@ -1,17 +1,53 @@
 #!/bin/bash
 
-#extract dataset metadata
-tar xvzf ./input/dataset.tar.gz -C ./input
+# Variable Declarations
+INPUT_VOL="/HOTELS-50K/input"
+IMAGE_VOL="/HOTELS-50K/images"
+PRETRAINED_VOL="/HOTELS-50K/pretrained_model"
+PRETRAINED_DIR="$PRETRAINED_VOL/hotels50k_snapshot"
+DATASET_DIR="$INPUT_VOL/dataset" 
+TESTIMAGE_DIR="$IMAGE_VOL/test"
+TESTIMAGE_URL="https://cs.slu.edu/~stylianou/images/hotels-50k/test.tar.lz4"
+PRETRAINED_URL="https://www2.seas.gwu.edu/~astylianou/hotels50k/hotels50k_snapshot.tar.gz"
 
-#Extract test images
-lz4toolsCli -d ./images/test.tar.lz4
-tar xvf ./images/test.tar -C ./images
+#Extract dataset meta data to input directory.
+# Check if datasets directory exists
+if [ ! -d $DATASET_DIR ]; then
+	# wget -O $INPUT_VOL/dataset.tar.gz "$DATASETURL"
+	# if [ $? -eq 0 ]; then
+		tar xvf $INPUT_VOL/dataset.tar.gz -C $INPUT_VOL
+        echo "Dataset extracted to volume!"
+	# fi
+else
+	# Dataset directory already exists
+	echo "Dataset preloaded on volume"
+fi
 
-#Extract pretrianed model
-cd pretrained_model
-tar xzvf hotels50k_snapshot.tar.gz
+#download test images to images folder to preserve structure.
+# Check if test images exist
+if [ ! -d $TESTIMAGE_DIR ]; then
+	if [ ! -f $IMAGE_VOL/test.tar.lz4 ]; then
+		wget "$TESTIMAGE_URL" -P $IMAGE_VOL/ --no-check-certificate
+        echo "Test images downloaded!"
+	fi
+    lz4 -d -v $IMAGE_VOL/test.tar.lz4 $IMAGE_VOL/test.tar
+    tar xvf $IMAGE_VOL/test.tar -C $IMAGE_VOL/
+    echo "Test images extracted to volume!"
+else
+	# Dataset directory already exists
+	echo "Test images directory found on volume"
+fi
 
-cd ..
-
-
-
+#Get pretrained model
+# Check if pretrained model exist
+if [ ! -d $PRETRAINED_DIR ]; then
+	if [ ! -f $PRETRAINED_VOL/hotels50k_snapshot.tar.gz ]; then
+		wget "$PRETRAINED_URL" -P $PRETRAINED_VOL --no-check-certificate
+        echo "Pretrained model downloaded!"
+	fi
+    tar xvf $PRETRAINED_VOL/hotels50k_snapshot.tar.gz -C $PRETRAINED_VOL/
+    echo "Pretrained model extracted to volume!"
+else
+	# Dataset directory already exists
+	echo "Pretrained model directory found on volume"
+fi
