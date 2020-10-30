f559eb00878d7ae0b063a9f0e8d9f67d3152a820
Ogecho Onuoha <oge.blessing@live.com>
2020-10-30 21:32:41 +0000

    Working version of convert knn to probs

--- evaluate/convert_knn_to_probabilities.py
+++ evaluate/convert_knn_to_probabilities.py
@@ -10,8 +10,8 @@ def main(input_file,output_file):
     test_id_to_class = id_to_class_parser(os.path.join(dirname,'..','input/dataset/test_set.csv'))
     train_id_to_class = id_to_class_parser(os.path.join(dirname,'..','input/dataset/train_set.csv'))
 
+    #Get unique hotel ids
     hotel_class_ids = np.unique(list(train_id_to_class.values()))
-
 
     with open(input_file) as in_f:
         with open(output_file,'w') as out_f:
@@ -23,10 +23,10 @@ def main(input_file,output_file):
                 if len(row) < 101:
                     print("Expected each row to contain a query image ID and at least 100 result image IDs. Failed at line: " + str(lnNum))
                     break
-                for idx in range(len(row[1:])):
+                for idx in range(len(row[1:])): #loop from second to the last id of similar images
                     try:
-                        result_class = train_id_to_class[int(row[1+idx])]
-                        result_class_ind = np.where(hotel_class_ids==result_class)[0]#[0]
+                        result_class = train_id_to_class[int(row[1+idx])] #get the class of each
+                        result_class_ind = np.where(hotel_class_ids==result_class)[0][0]
                         class_probabilities[result_class_ind] += 1.
                     except Exception as e:
                         print("Result  image ID ("+row[1+idx]+") in row " + str(lnNum) + " is unknown.")
@@ -36,7 +36,7 @@ def main(input_file,output_file):
                 non_zero_inds = np.where(class_probabilities>0)[0]
                 class_probabilities = class_probabilities / non_zero_inds.shape[0]
                 prob_str = row[0] + ','
-                prob_str += ','.join(['%i,%0.3f' % (c,p) for c,p in zip(hotel_class_ids[result_class_ind],class_probabilities[result_class_ind])])
+                prob_str += ','.join(['%i,%0.3f' % (c,p) for c,p in zip(hotel_class_ids[non_zero_inds],class_probabilities[non_zero_inds])])
                 prob_str += '\n'
 
                 out_f.writelines(prob_str)
