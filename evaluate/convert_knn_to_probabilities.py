0bcf018c7e8693da10d2cea691dec3a98fe6253c
Ogecho Onuoha <oge.blessing@live.com>
2020-10-30 16:42:53 +0000

    Fix the list and indexing bugs

--- evaluate/convert_knn_to_probabilities.py
+++ evaluate/convert_knn_to_probabilities.py
@@ -6,10 +6,12 @@ import sys
 from utils import id_to_class_parser
 
 def main(input_file,output_file):
+    dirname = os.path.dirname(__file__)
     test_id_to_class = id_to_class_parser(os.path.join(dirname,'..','input/dataset/test_set.csv'))
     train_id_to_class = id_to_class_parser(os.path.join(dirname,'..','input/dataset/train_set.csv'))
 
-    hotel_class_ids = np.unique(train_id_to_class.values())
+    hotel_class_ids = np.unique(list(train_id_to_class.values()))
+
 
     with open(input_file) as in_f:
         with open(output_file,'w') as out_f:
@@ -18,16 +20,17 @@ def main(input_file,output_file):
             for row in csv_reader:
                 query_image_id = int(row[0])
                 class_probabilities = np.zeros(hotel_class_ids.shape[0])
-                if len(row) != 101:
-                    print("Expected each row to contain a query image ID and 100 result image IDs. Failed at line: " + str(lnNum))
+                if len(row) < 101:
+                    print("Expected each row to contain a query image ID and at least 100 result image IDs. Failed at line: " + str(lnNum))
                     break
                 for idx in range(len(row[1:])):
                     try:
                         result_class = train_id_to_class[int(row[1+idx])]
-                        result_class_ind = np.where(hotel_class_ids==result_class)[0][0]
+                        result_class_ind = np.where(hotel_class_ids==result_class)[0]#[0]
                         class_probabilities[result_class_ind] += 1.
-                    except:
+                    except Exception as e:
                         print("Result  image ID ("+row[1+idx]+") in row " + str(lnNum) + " is unknown.")
+                        print(e)
                         break
 
                 non_zero_inds = np.where(class_probabilities>0)[0]
@@ -46,3 +49,5 @@ if __name__ == "__main__":
     input_path = args[1]
     output_path = args[2]
     main(input_path,output_path)
+
+
